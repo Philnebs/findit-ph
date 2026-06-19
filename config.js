f116b457c21b602c48ccb2115ffd605b83c8ae3d
Philnebs <ichinegbo@gmail.com>
2026-06-20 00:03:10 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,14 @@
 // config.js
-const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b31879424""; 
-const ADMIN_WHATSAPP = "2348107474626";
+export const firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "findit-ph-e1d0f.firebaseapp.com",
+  projectId: "findit-ph-e1d0f",
+  storageBucket: "findit-ph-e1d0f.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
-// CHANGE PRICE HERE ONLY - Affects entire site
-const UNLOCK_PRICE = 100; // Change to 200, 300, 500 anytime you want
+// Your Paystack stuff can stay below if you want
+export const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b318";
+export const ADMIN_WHATSAPP = "2348187474626";
+export const UNLOCK_PRICE = 100; // Change to 200, 300, 500 anytime you want
