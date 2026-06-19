46384d69dd95c241375f5727580ff60bf3a4f408
Philnebs <ichinegbo@gmail.com>
2026-06-19 12:03:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b31879424"; // Your real key
-const ADMIN_WHATSAPP = "2348012345678"; // Change to YOUR WhatsApp number
+const ADMIN_WHATSAPP = "2348107474626"; // Change to YOUR WhatsApp number
 const UNLOCK_PRICE = 100; // You can change this anytime to 200, 500, etc
