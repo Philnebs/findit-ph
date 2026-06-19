05b206258086f3946744cae59fc19e937eaf7528
Philnebs <ichinegbo@gmail.com>
2026-06-19 13:46:58 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js - FINDIT-PH Database Connection
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, query, where } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+// REPLACE WITH YOUR FIREBASE CONFIG FROM PROJECT SETTINGS
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export { collection, addDoc, getDocs, updateDoc, doc, query, where, ref, uploadBytes, getDownloadURL };
