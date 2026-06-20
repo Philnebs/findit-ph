02272fdec7e42a2df4b3fa36269f30e3ddc6e28b
Philnebs <ichinegbo@gmail.com>
2026-06-20 01:29:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,5 +11,5 @@ export const firebaseConfig = {
 
 // Your Paystack stuff can stay below if you want
 export const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b318";
-export const ADMIN_WHATSAPP = "2348187474626";
+export const ADMIN_WHATSAPP = "2348107474626";
 export const UNLOCK_PRICE = 100; // Change to 200, 300, 500 anytime you want
