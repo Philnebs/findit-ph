deec44fe31654f037d0934216e2122329e8216e1
Philnebs <ichinegbo@gmail.com>
2026-06-23 07:48:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyB3PfjfnBDe6atBpDuqIQg7lISUKu2YuFw",
+  apiKey: "AIzaSyB3PfJfnBDe6atBpDuqIQg7IISUKu2YuFw",
   authDomain: "findit-ph-e1d0f.firebaseapp.com",
   databaseURL: "https://findit-ph-e1d0f-default-rtdb.firebaseio.com",
   projectId: "findit-ph-e1d0f",
@@ -10,7 +10,7 @@ export const firebaseConfig = {
 };
 
 // Your Paystack stuff can stay below if you want
-//export const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b318";
+//export const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b318"; 
 export const ADMIN_WHATSAPP = "2348107474626";
 export const UNLOCK_PRICE = 100; // Change to 200, 300, 500 anytime you want
-export const FLWPUBK_TEST-e7670aaf7e8a5e7bb230eb156189ec38-X
+export const FLUTTERWAVE_PUBLIC_KEY = "FLWPUBK_TEST-e7670aaf7e8a5e7bb230eb156189ec38-X";
