a6e933deda95511900b5d30a2b5332aa8a5f4b9e
Philnebs <ichinegbo@gmail.com>
2026-06-23 18:23:34 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
 // config.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyB3PfJfnBDe6atBpDuqIQg7IISUKu2YuFw",
-  authDomain: "findit-ph-e1d0f.firebaseapp.com",
-  databaseURL: "https://findit-ph-e1d0f-default-rtdb.firebaseio.com",
-  projectId: "findit-ph-e1d0f",
-  storageBucket: "findit-ph-e1d0f.firebasestorage.app",
-  messagingSenderId: "18398015033",
-  appId: "1:18398015033:web:53a46e7e32b7f6cdeb27e8"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Your Paystack stuff can stay below if you want
-//export const PAYSTACK_PUBLIC_KEY = "pk_test_801bdf0827727162d12ad6750620ac3b318"; 
+//export const PAYSTACK_PUBLIC_KEY = "";
 export const ADMIN_WHATSAPP = "2348107474626";
 export const UNLOCK_PRICE = 100; // Change to 200, 300, 500 anytime you want
-export const FLUTTERWAVE_PUBLIC_KEY =FLWPUBK-8e1e872f6b9446778c604da939ff3942-X ;
+export const FLUTTERWAVE_PUBLIC_KEY = ""; // LEAVE EMPTY. Netlify go inject am.
+
+// Don't paste FLWSECK_xxxxx here o! Leave am empty ;
